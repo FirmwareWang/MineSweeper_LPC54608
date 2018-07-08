8ffd3840fe150cf7aef697148ef8a4ae973cfc1a
JinliWangKolmostar <jingli.wang@kolmostar.com>
2018-07-08 15:48:50 +0800

    new file and folder for game engine

---
+++ MineSweeper_LPC54608/source/game_engine/inc/simple_engine.h
@@ -0,0 +1,13 @@
+/*
+ * simple_engine.h
+ *
+ *  Created on: Jul 8, 2018
+ *      Author: wangjingli
+ */
+
+#ifndef GAME_ENGINE_INC_SIMPLE_ENGINE_H_
+#define GAME_ENGINE_INC_SIMPLE_ENGINE_H_
+
+
+
+#endif /* GAME_ENGINE_INC_SIMPLE_ENGINE_H_ */
---
+++ MineSweeper_LPC54608/source/game_engine/src/simple_engine.c
@@ -0,0 +1,8 @@
+/*
+ * simple_engine.c
+ *
+ *  Created on: Jul 8, 2018
+ *      Author: wangjingli
+ */
+
+
